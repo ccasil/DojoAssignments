1c7c0ca62f7819b840c52f38fdc9c6aed2468a3b
Cesar Kyle Casil <ccasil@ucsc.edu>
2018-02-08 19:55:39 -0800

    @ home work

--- Python/Flask/DojoSurvey/server.py
+++ Python/Flask/DojoSurvey/server.py
@@ -8,7 +8,15 @@ def entername():
 
 @app.route('/process', methods=['POST'])
 def process():
-	name = request.form['formname']
-	return render_template('/process.html', processname=name)
-	# return redirect('/')
+	print "in process route"
+	name = request.form['name']
+	location = request.form['location']
+	language = request.form['language']
+	description = request.form['description']
+	return render_template('/process.html', name=name, location=location, language=language, description=description )
+	# return redirect('/result') #never render post
+
+# @app.route('/result')
+# def result():
+# 	return render_template('/process.html', name=name, location=location, language=language, description=description )
 app.run(debug=True)
