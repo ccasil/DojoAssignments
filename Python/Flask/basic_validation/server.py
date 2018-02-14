ca097c9e48372d253706e65c98c94ada7d2db9f3
Cesar <cesarkylecasil@gmail.com>
2018-02-13 22:33:01 -0800

    changed file on desktop

--- Python/Flask/basic_validation/server.py
+++ Python/Flask/basic_validation/server.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, redirect, request, session, flash
 app = Flask(__name__)
 app.secret_key = 'KeepItSecretKeepItSafe'
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
 @app.route('/process', methods=['Post'])
 def process():
   if len(request.form['name']) < 1:
@@ -11,4 +13,5 @@ def process():
   else:
     flash("Success! Your name is {}".format(request.form['name'])) # just pass a string to the flash function
   return redirect('/') # either way the application should return to the index and display the message
+  
 app.run(debug=True)
