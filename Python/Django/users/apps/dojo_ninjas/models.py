3239b2dda56a37afb8f44ec0e5747433456878ba
Cesar Kyle Casil <ccasil@ucsc.edu>
2018-02-20 17:56:13 -0800

    end of the day

--- Python/Django/users/apps/dojo_ninjas/models.py
+++ Python/Django/users/apps/dojo_ninjas/models.py
@@ -8,6 +8,7 @@ class Dojo(models.Model):
 	name = models.CharField(max_length = 255)
 	city = models.CharField(max_length = 255)
 	state = models.CharField(max_length = 255)
+	desc = models.TextField(default="")
 
 class Ninja(models.Model):
 	first_name = models.CharField(max_length = 255)
