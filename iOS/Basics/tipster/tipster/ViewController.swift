577e12c2bbed1a5c8e304a024b62ceded8ce74d5
ccasil <cesarkylecasil@gmail.com>
2018-03-09 15:18:13 -0800

    start of tipster

---
+++ iOS/Basics/tipster/tipster/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  tipster
+//
+//  Created by Cesar Casil on 3/9/18.
+//  Copyright © 2018 Cesar Casil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var amountTotalLabel: UILabel!
+    @IBOutlet weak var percentLabel0: UILabel!
+    @IBOutlet weak var percentLabel1: UILabel!
+    @IBOutlet weak var percentLabel2: UILabel!
+    @IBOutlet weak var amountLabel0: UILabel!
+    @IBOutlet weak var amountLabel1: UILabel!
+    @IBOutlet weak var amountLabel2: UILabel!
+    @IBOutlet weak var totalLabel0: UILabel!
+    @IBOutlet weak var totalLabel1: UILabel!
+    @IBOutlet weak var totalLabel2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
