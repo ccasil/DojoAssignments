f663a4c6456ce5c6c8ca02980180822a6e2df37d
ccasil <cesarkylecasil@gmail.com>
2018-03-11 18:25:55 -0700

    tipster

--- iOS/Basics/tipster/tipster/ViewController.swift
+++ iOS/Basics/tipster/tipster/ViewController.swift
@@ -19,16 +19,121 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel0: UILabel!
     @IBOutlet weak var totalLabel1: UILabel!
     @IBOutlet weak var totalLabel2: UILabel!
+    @IBOutlet weak var groupsizeLabel: UILabel!
+    
+    var amount = ""
+    
+    @IBAction func button9(_ sender: UIButton) {
+        amount += "9"
+        new()
+    }
+    @IBAction func button8(_ sender: UIButton) {
+        amount += "8"
+        new()
+    }
+    @IBAction func button7(_ sender: UIButton) {
+        amount += "7"
+        new()
+    }
+    @IBAction func button6(_ sender: UIButton) {
+        amount += "6"
+        new()
+    }
+    @IBAction func button5(_ sender: UIButton) {
+        amount += "5"
+        new()
+    }
+    @IBAction func button4(_ sender: UIButton) {
+        amount += "4"
+        new()
+    }
+    @IBAction func button3(_ sender: UIButton) {
+        amount += "3"
+        new()
+    }
+    @IBAction func button2(_ sender: UIButton) {
+        amount += "2"
+        new()
+    }
+    @IBAction func button1(_ sender: UIButton) {
+        amount += "1"
+        new()
+    }
+    @IBAction func button0(_ sender: UIButton) {
+        if amount != "" {
+            amount += "0"
+            new()
+        }
+    }
+    
+    var count: Int = 0
+    @IBAction func buttondec(_ sender: UIButton) {
+        count += 1
+        if count == 1{
+            amount += "."
+            new()
+        }
+    }
+    @IBAction func buttonclear(_ sender: UIButton) {
+        amount = ""
+        count = 0
+        amountTotalLabel.text = "0"
+        percentLabel0.text = "0%"
+        amountLabel0.text = "0.00"
+        totalLabel0.text = "0.00"
+        percentLabel1.text = "5%"
+        amountLabel1.text = "0.00"
+        totalLabel1.text = "0.00"
+        percentLabel2.text = "10%"
+        amountLabel2.text = "0.00"
+        totalLabel2.text = "0.00"
+    }
+    
+    var percent = 0
+    @IBAction func tipSlider(_ sender: UISlider) {
+        let slidep: Int = Int(sender.value)
+        percent = slidep
+        new()
+    }
+    var group: Int = 1
+    @IBAction func groupSlider(_ sender: UISlider) {
+        let slideg = Int(sender.value)
+        group = slideg
+        new()
+    }
+    
+    func new(){
+        amountTotalLabel.text = amount
+        
+        percentLabel0.text = String(percent) + "%"
+        percentLabel1.text = String(percent + 5) + "%"
+        percentLabel2.text = String(percent + 10) + "%"
+        let amounts: String? = amount
+        if let unwrappedamount = amounts {
+            
+            let amountd = Double(unwrappedamount)!
+            
+            let beforetotal0 = Double(amountd) * (Double(percent) * 0.01)
+            amountLabel0.text = String(format: "%.2f", beforetotal0 / Double(group))
+            totalLabel0.text = String(format: "%.2f", (amountd + beforetotal0) / Double(group))
+            
+            let beforetotal1 = Double(amountd) * (Double(percent + 5) * 0.01)
+            amountLabel1.text = String(format: "%.2f", beforetotal1 / Double(group))
+            totalLabel1.text = String(format: "%.2f", (amountd + beforetotal1) / Double(group))
+            
+            let beforetotal2 = Double(amountd) * (Double(percent + 10) * 0.01)
+            amountLabel2.text = String(format: "%.2f", beforetotal2 / Double(group))
+            totalLabel2.text = String(format: "%.2f", (amountd + beforetotal2) / Double(group))
+            
+            groupsizeLabel.text = String(group)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
