e9d4a13171bbb3b6487e9bd055f7fe326da46825
ccasil <cesarkylecasil@gmail.com>
2018-04-09 21:39:11 -0700

    updated basic mongoose

--- MEAN/MongoDB/basic_mongoose_app/server.js
+++ MEAN/MongoDB/basic_mongoose_app/server.js
@@ -15,10 +15,13 @@ var mongoose = require('mongoose')
 // This is how we connect to the mongodb database using mongoose -- "basic_mongoose" is the name of
 //   our db in mongodb -- this should match the name of the db you are going to use for your project.
 mongoose.connect('mongodb://localhost/basic_mongoose');
+// to make a model, you can first define a schema, which is just the BLUEPRINT for a model
 var UserSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-})
+    first_name: { type: String, required: true, minlength: 6 },
+    last_name: { type: String, required: true, maxlength: 20 },
+    age: { type: Number, min: 1, max: 150 },
+    email: { type: String, required: true }
+}, { timestamps: true });
 mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
 var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
 app.set('views', path.join(__dirname, './views'));
@@ -29,8 +32,6 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
     User.find({}, function (err, users) {
-        let name = users.name
-        let age = users.age
         // This is the method that finds all of the users from the database
         // Notice how the first parameter is the options for what to find and the second is the
         //   callback function that has an error (if any) and all of the users
@@ -38,7 +39,7 @@ app.get('/', function (req, res) {
         //   happen inside of this callback for it to be synchronous 
         // Make sure you handle the case when there is an error, as well as the case when there is no error
     })
-    res.render('index', {name: name, age: age});
+    res.render('index');
 })
 // Use native promises
 mongoose.Promise = global.Promise;
@@ -49,17 +50,16 @@ mongoose.Promise = global.Promise;
 app.post('/users', function (req, res) {
     console.log("POST DATA", req.body);
     // create a new User with the name and age corresponding to those from req.body
-    var user = new User({ name: req.body.name, age: req.body.age });
+    var user = new User({ first_name: req.body.first_name, last_name: req.body.last_name, age: req.body.age, email: req.body.email });
     // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
-    user.save(function (err) {
-        // if there is an error console.log that something went wrong!
-        if (err) {
-            console.log('something went wrong');
-        } else { // else console.log that we did well and then redirect to the root route
-            console.log('successfully added a user!');
+    user.save(function(err){
+        if(err){
+            res.render('index', {errors: user.errors})
+        }
+        else {
             res.redirect('/');
         }
-    })
+    });
 })
 
 
